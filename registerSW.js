75d119eb3d585dc0b863d2c64b72cb18fc78c578
Jorge Coy <jorgecoy@gmail.com>
2025-11-23 15:11:26 -0500

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aileer-lectura-accesible/sw.js', { scope: '/aileer-lectura-accesible/' })})}
